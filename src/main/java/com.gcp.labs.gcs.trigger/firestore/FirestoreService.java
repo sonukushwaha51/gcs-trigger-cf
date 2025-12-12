ccd916e63519dfacba12f4ea4d7e62bcd6bbf92e
sonukushwaha51 <sonukushwaha51@gmail.com>
2025-12-13 01:02:41 +0530

    fix timestamp issue

--- src/main/java/com.gcp.labs.gcs.trigger/firestore/FirestoreService.java
+++ src/main/java/com.gcp.labs.gcs.trigger/firestore/FirestoreService.java
@@ -1,13 +1,18 @@
 package com.gcp.labs.gcs.trigger.firestore;
 
+import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteResult;
 import com.google.events.cloud.storage.v1.StorageObjectData;
 import com.google.inject.Inject;
 import com.google.inject.name.Named;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.Instant;
 import java.util.Date;
+import java.util.concurrent.ExecutionException;
 
+@Slf4j
 public class FirestoreService {
 
     private final Firestore firestore;
@@ -28,6 +33,14 @@ public class FirestoreService {
         imageModel.setImageType(storageObjectData.getContentType());
         imageModel.setCreatedTime(Date.from(Instant.ofEpochMilli(storageObjectData.getTimeCreated().getSeconds() * 1000)));
 
-        firestore.collection(imageCollection).document(storageObjectData.getName()).set(imageModel);
+        ApiFuture<WriteResult> future = firestore.collection(imageCollection).document(storageObjectData.getName()).set(imageModel);
+        try {
+            WriteResult result = future.get();
+            log.info("Successfully wrote the data to firestore: {}", result.getUpdateTime());
+        } catch (InterruptedException | ExecutionException e) {
+            log.error("Exception occurred while writing to firestore", e);
+            throw new RuntimeException(e);
+        }
+
     }
 }
