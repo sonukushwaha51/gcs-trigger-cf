9a4d42042bf03e7e06f9ee23c925fe40a4ecbf5d
sonukushwaha51 <sonukushwaha51@gmail.com>
2025-12-13 00:09:22 +0530

    debug

--- src/main/java/com.gcp.labs.gcs.trigger/properties/PropertiesModule.java
+++ src/main/java/com.gcp.labs.gcs.trigger/properties/PropertiesModule.java
@@ -3,6 +3,7 @@ package com.gcp.labs.gcs.trigger.properties;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
+import com.google.inject.Scopes;
 import com.google.inject.Singleton;
 import com.google.inject.name.Named;
 import lombok.extern.slf4j.Slf4j;
@@ -14,9 +15,13 @@ import java.util.Properties;
 @Slf4j
 public class PropertiesModule extends AbstractModule {
 
+    @Override
+    public void configure() {
+        this.bind(PropertiesService.class).in(Scopes.SINGLETON);
+    }
+
     @Provides
     @Singleton
-    @Named("properties")
     public Properties provideProperties() {
         Properties properties = new Properties();
         try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream("application.properties")) {
